d9b11e9e51cfdd205a133dd0e764c1f8c616590e
Eugene Fedorov <uptiik02@gmail.com>
2025-01-15 17:14:46 +0300

    setup admin UI for likes

--- property/models.py
+++ property/models.py
@@ -48,6 +48,7 @@ class Flat(models.Model):
         blank=True,
         db_index=True)
     new_building = models.BooleanField(default=None, blank=True, null=True)
+    liked_by = models.ManyToManyField(User, verbose_name='Кто лайкнул', related_name="likes")
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
